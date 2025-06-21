45fbeb52893fecb5df0ea84903be63764c2f05eb
VINOTH KUMAR K B <121332778+59VINOTHKUMARKB@users.noreply.github.com>
2025-06-21 23:10:44 +0530

    Recent Commit after some code changes

--- Upskilling Phase Learning/Java/LambdaSort.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class LambdaSort {
-    public static void main(String[] args) {
-        List<String> names = Arrays.asList("Zara", "Bob", "Anna", "Mike");
-        Collections.sort(names, (a, b) -> a.compareToIgnoreCase(b));
-        System.out.println("Sorted names: " + names);
-    }
-}
--- Upskilling Phase Learning/Java/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package com.utils;
-
-public class StringUtil {
-    public static String toUpper(String str) {
-        return str.toUpperCase();
-    }
-}
