1681887c69f4fcc058ba886c91d9f7e2e6af2704
VINOTH KUMAR K B <121332778+59VINOTHKUMARKB@users.noreply.github.com>
2025-06-22 10:20:55 +0530

    Update Order.java

--- Week_1 Assessments/DSA/ECommerceSort/Order.java
+++ Week_1 Assessments/DSA/ECommerceSort/Order.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Week1.DSA.ECommerceSort;
 
-/**
- *
- * @author EDITH
- */
 public class Order {
     public int orderId;
     public String customerName;
