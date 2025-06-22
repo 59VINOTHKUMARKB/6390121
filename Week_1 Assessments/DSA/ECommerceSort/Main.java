97c794692ebcc9b87fbc38a7f8b51ad433f87731
VINOTH KUMAR K B <121332778+59VINOTHKUMARKB@users.noreply.github.com>
2025-06-22 10:20:22 +0530

    Update Main.java

--- Week_1 Assessments/DSA/ECommerceSort/Main.java
+++ Week_1 Assessments/DSA/ECommerceSort/Main.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Week1.DSA.ECommerceSort;
-
-/**
- *
- * @author EDITH
- */
 public class Main {
     public static void main(String[] args) {
         Order[] orders = {
